ddc54c398c111146dc877781c08758bd0ef8443a
Volodymyr Shutov <forshutov@gmail.com>
2024-08-21 18:08:54 +0400

    Solution

--- task.ps1
+++ task.ps1
@@ -1,1 +1,12 @@
-# Write your code here
+$disks = Get-AzDisk
+$resultJson = "./result.json"
+
+$matchingDisks = @()
+
+foreach ($disk in $disks) {
+    if ($disk.DiskState -eq "Unattached" -or $null -eq $disk.ManagedBy) {
+        $matchingDisks += $disk
+    }
+}
+
+$matchingDisks | ConvertTo-Json | Out-File -Path $resultJson
